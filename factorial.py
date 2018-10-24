12120ffc1596121b33a685aab7ea7d8600e0508c
sumedh-kulkarni <44223857+sumedh-kulkarni@users.noreply.github.com>
2018-10-24 09:52:12 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,18 @@
+#name Pratik Badhe
+#roll no. 02
+#gr no 11810754
+
+
+def facto(x): 
+   if x==0:
+      print("1")
+   elif x==1:
+      print("1")
+   else:
+      multi=1
+      for i  in range (2,x+1,1):
+        multi=multi*i
+      print(multi)
+x=int(input("enter a number"))
+m=facto(x)
+
