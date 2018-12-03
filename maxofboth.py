46ca809c6a866992ea23cc5d05ccaa2155f0aa70
sumedh-kulkarni <44223857+sumedh-kulkarni@users.noreply.github.com>
2018-12-03 18:25:16 +0530

    Create maxofboth.py

---
+++ maxofboth.py
@@ -0,0 +1,14 @@
+#MAX OF BOTH
+#SUMEDH KULKARNI
+#M60
+def max(x,y):
+ if x>y:
+  return x
+ elif y>x:
+  return y
+ else:
+  print("both are equal")
+x=int(input())
+y=int(input())
+result=max(x,y)
+print(result)
