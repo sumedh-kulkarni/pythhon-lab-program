720ce818ac8170b97a3ed3a9eb08702df0509868
sumedh-kulkarni <44223857+sumedh-kulkarni@users.noreply.github.com>
2018-12-03 17:50:39 +0530

    temp.py

---
+++ temp.py
@@ -0,0 +1,9 @@
+M46
+x=int(input("Enter temperature:"))#INPUT STATEMENT
+y=input("is it in 'celcius' or 'temperatur'")#ASKING WHETHER IT IS IN CELSIUS OR FARENITE
+if y is 'celsius':#CONITIONAL STATEMENT
+ f=9*x/5 + 32
+ print(f)
+else:
+ c=5/9*(x-32)
+print(c)
