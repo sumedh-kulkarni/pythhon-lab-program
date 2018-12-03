87786b2244d4f8d6d9778c9a58b0b90b0a9a4429
sumedh-kulkarni <44223857+sumedh-kulkarni@users.noreply.github.com>
2018-12-03 18:20:07 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,15 @@
+#LEAP YEAR
+#SUMEDH KULKARNI
+#M60
+y=int(input("enter the year"))
+if y%4==0:
+ if y%100==0:
+  if y%400==0:
+   print("leap year")
+  else:
+   print("not a leap year")
+ else:
+  print("a leap year")
+else:
+ print("not a leap year")
+ 
